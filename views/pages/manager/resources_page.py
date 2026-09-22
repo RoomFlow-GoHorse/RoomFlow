4a6a4cd373d166ca172d382aa9aec01a75ce9302
Ligia <ligia.nascimento2020@gmail.com>
2026-09-22 08:56:48 -0400

    remove: excluir arquivos nao utilizados

--- views/pages/manager/resources_page.py
+++
@@ -1,6 +0,0 @@
-from views.components.ui_components import empty_state, page_header
-
-
-def resources(user):
-    page_header("Recursos", "Consulte os recursos dos espacos.")
-    empty_state("Recursos indisponiveis", "Esta visao ainda nao possui dados no ambiente mockado.")
